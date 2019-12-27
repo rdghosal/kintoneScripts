b36faf594cde84f65bc5544efc91337666519835
Rahul D. Ghosal <rdghosal@gmail.com>
2019-12-26 19:58:08 -0500

    Manually tested and debugged

--- mitsumoriSearchBar.js
+++ mitsumoriSearchBar.js
@@ -10,42 +10,79 @@ kintone.events.on("app.record.index.show", event => {
 
     // Create a <select> element to pick a field for query
     const fieldSelector = document.createElement("select");
-    fields.forEach(field => fieldSelector.options.add(field));
-    
-    const searchButton = document.createElement("button");
-    searchButton.innerHTML = "Click me!";
+    fieldSelector.setAttribute("id", "field-selector");
 
+    // Add each field as a search option
+    fields.forEach(field => {
+        const option = document.createElement("option");
+        if (field === "プロジェクト名称") {
+            option.selected = true; // Select default
+        }
+        option.innerHTML = field;
+        fieldSelector.appendChild(option);
+    });
+    
     // Create a <input> element to allow text input (searchbar)
     const searchBar = document.createElement("input");
     searchBar.setAttribute("type", "test");
-    searchBar.setAttribute("placeholder", "Type something!");
+    searchBar.setAttribute("id", "search-bar");
+    searchBar.setAttribute("placeholder", "キーワード入力");
     searchBar.setAttribute("autofocus", "on");
     searchBar.setAttribute("autocomplete", "on");
 
-    // Get target field 
-    const targetField = fieldSelector.value
-    const query = searchBar.value
-    searchButton.onclick = searchRecords(targetField, query, records);
+    // Make button that initiates search algorithm upon click
+    const searchButton = document.createElement("button");
+    searchButton.innerHTML = "検索";
+    searchButton.id = "search-button";
+
+    // Add a help button
+    const helpButton = document.createElement("button");
+    helpButton.innerHTML = "Help";
+    helpButton.onclick = () => alert("フィールドを選択し、キーワードを検索してください。");
+
+    // Grab space in app and insert new DOM elements
+    const menuSpace = kintone.app.getHeaderMenuSpaceElement();
+    menuSpace.appendChild(fieldSelector);
+    menuSpace.appendChild(searchBar);
+    menuSpace.appendChild(searchButton);
+    menuSpace.appendChild(helpButton);
+
+    searchButton.addEventListener("click", () => searchRecords(records));
 });
 
-const searchRecords = (field, query, records) => {
-    const filteredRecords = records.filter(record => filterRecords(field, query, record) )
-    // TODO: process filteredRecords
+function searchRecords(records) {
+    const field = document.getElementById("field-selector").value;
+    const query = document.getElementById("search-bar").value;
+    const filteredRecords = records.filter(record => {
+        return filterRecords(field, query, record);
+    });
+
+    // Parse current URL 
+    let baseUrl = window.location.href;
+    const end = baseUrl.indexOf(".com") + 5; // Takes up to forward slash
+    baseUrl = baseUrl.slice(0, end); 
+
+    // Make query string for each record
+    // navigate to each found record each as separate search result
+    filteredRecords.forEach(rec => {
+        window.open(baseUrl + `/k/search?keyword=${rec[field].value}&sortOrder=DATETIME&app=${kintone.app.getId()}`);
+    });
 }
 
-const filterRecords = (field, query, record) => {
+function filterRecords(field, query, record) {
     let foundMatch = false;
-    if (typeof record[field] === "string") {
+    if (typeof record[field].value === "string") {
         // Fuzzy matching for type string
-        const index = record[field].indexOf(query);
+        const index = record[field].value.indexOf(query);
         if (index > -1) {
             foundMatch = true;
         }
-    } else if (typeof record[field] === "number") {
+    } else if (typeof record[field].value === "number") {
         // Hard matching for type number
-        if (record[field] === query) {
+        if (record[field].value === query) {
             foundMatch = true;
         }
     }
+    console.log(foundMatch);
     return foundMatch;
 }  
